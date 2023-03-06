332e133194a7dcf48773dc0fa7428e69c1f63889
Jochen723 <jonaskortum@googlemail.com>
2023-03-06 16:26:42 +0100

    initial Commit

---
+++ rezeptobot/src/main/java/de/webapplication/rezeptobot/RezeptobotApplication.java
@@ -0,0 +1,13 @@
+package de.webapplication.rezeptobot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RezeptobotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RezeptobotApplication.class, args);
+	}
+
+}
