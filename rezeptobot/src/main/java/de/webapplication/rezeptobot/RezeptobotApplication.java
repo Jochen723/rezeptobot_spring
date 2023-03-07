d9ea080d264b2cbe831ba0d06c7ff403eb903359
Jochen723 <jonaskortum@googlemail.com>
2023-03-07 22:58:08 +0100

    RezeptobotApplication formatiert

--- rezeptobot/src/main/java/de/webapplication/rezeptobot/RezeptobotApplication.java
+++ rezeptobot/src/main/java/de/webapplication/rezeptobot/RezeptobotApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RezeptobotApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RezeptobotApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(RezeptobotApplication.class, args);
+  }
 }
