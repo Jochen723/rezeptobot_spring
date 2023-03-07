3ef56f9b3be51603cb127e8631f6b9a362b3c243
Jochen723 <jonaskortum@googlemail.com>
2023-03-07 23:43:39 +0100

    Diverses Refactoring

--- rezeptobot/src/main/resources/static/js/login.js
+++ rezeptobot/src/main/resources/static/js/login.js
@@ -1,49 +1,19 @@
-$(document).ready(function(){
-    
-    $(document).ajaxSend(function() {
-        $("#overlay").fadeIn(300);　
-    });
+function checkLogin() {
+  var validationSuccessful = true,
+        mail = document.getElementById("mail").value;
 
-    $(".btn-login").click(function() {
+  if (!validateEmail(mail)) {
+    validationSuccessful = false;
+    document.getElementById("emailFailure").innerHTML =
+      "Bitte geben Sie eine valide E-Mail Adresse ein.";
+    document.getElementById("emailFailure").style.display = "inline";
+  } else {
+    document.getElementById("emailFailure").style.display = "none";
+  }
+}
 
-        var mail = document.getElementById('mail').value;
-        var pw = document.getElementById('passwort').value;
-
-        if (validateEmail(mail)) {
-            document.getElementById("loginFailed").style.display = "none";
-            $.ajax({
-                type:'GET',
-                url:'backend/getLogin.php',
-                dataType: "json",
-                data: {
-                    'mail': mail,
-                    'pass': pw,
-                },
-                error: function () {
-                    document.getElementById("loginFailed").innerHTML = "Benutzername oder Passwort falsch.";
-                        document.getElementById("loginFailed").style.display = "inline";  
-                    $("#overlay").fadeOut(300);
-                  }
-            }).done(function(data) {
-                setTimeout(function(){
-                    if (data.nutzernameVorhanden === false || data.passwortKorrekt == false) {
-                        document.getElementById("loginFailed").innerHTML = "Benutzername oder Passwort falsch.";
-                        document.getElementById("loginFailed").style.display = "inline";  
-                    } else {
-                        window.location.href = "module/startseite/masken/startseite";
-                    }
-                    $("#overlay").fadeOut(300);
-                },500);
-              });
-          } else {
-            document.getElementById("loginFailed").innerHTML = "Bitte geben Sie eine valide E-Mail Adresse ein."; 
-            document.getElementById("loginFailed").style.display = "inline";        
-          }
-    });
-
-    function validateEmail(mail) {
-        var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-
-        return mail.match(validRegex);
-    }
+function validateEmail(email) {
+  var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return re.test(email);
+}
 });
