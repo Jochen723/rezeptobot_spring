68300596911383085265d46b8b157402dfa3820e
Jochen723 <jonaskortum@googlemail.com>
2023-03-09 23:37:37 +0100

    Es können jetzt Rezeptbilder hochgeladen werden

--- rezeptobot/src/main/java/de/webapplication/rezeptobot/models/enums/EinheitenEnum.java
+++ rezeptobot/src/main/java/de/webapplication/rezeptobot/models/enums/EinheitenEnum.java
@@ -4,4 +4,11 @@ public enum EinheitenEnum {
   BECHER,
   BEUTEL,
   BLATT_BLAETTER,
+  BUND,
+  DOSE_DOSEN,
+  ESSLOEFFEL,
+  GLAS_GLAESER,
+  GRAMM,
+  HANDVOLL,
+  KUGEL_KUGELN,
 }
---
+++ rezeptobot/src/main/resources/static/js/neuesRezept.js
@@ -0,0 +1,66 @@
+function addNewIngredient() {
+  var nodeAvailable = true,
+    zaehler = 1,
+    node = document.getElementById("zutatenrow_1"),
+    clonedNode = node.cloneNode(true);
+
+  do {
+    nodeAvailable =
+      document.getElementById("zutatenrow" + "_" + zaehler) != null;
+    if (nodeAvailable) {
+      zaehler = zaehler + 1;
+    }
+  } while (nodeAvailable);
+
+  clonedNode.setAttribute("id", "zutatenrow_" + zaehler);
+  clonedNode.querySelector("#anzahlSelect1").name =
+    "rezeptzutaten[" + (zaehler - 1) + "].anzahl";
+
+  clonedNode.querySelector("#einheitSelect1").name =
+    "rezeptzutaten[" + (zaehler - 1) + "].einheit";
+  clonedNode.querySelector("#zutatSelect1").name =
+    "rezeptzutaten[" + (zaehler - 1) + "].bezeichnung";
+
+  clonedNode.querySelector("#minusbtn1").id = "minusbtn" + zaehler;
+
+  document.getElementById("zutatenreihe").appendChild(clonedNode);
+}
+
+function removeNewIngredient(clicked_id) {
+  var nodeAvailable = true,
+    zaehler = Number(clicked_id.substr(clicked_id.length - 1));
+
+  document.getElementById("zutatenrow" + "_" + zaehler).remove();
+
+  do {
+    nodeAvailable =
+      document.getElementById("zutatenrow" + "_" + (zaehler + 1)) != null;
+    if (nodeAvailable) {
+      document
+        .getElementById("zutatenrow" + "_" + (zaehler + 1))
+        .querySelector("#anzahlSelect1").name =
+        "rezeptzutaten[" + (zaehler - 1) + "].anzahl";
+      document
+        .getElementById("zutatenrow" + "_" + (zaehler + 1))
+        .querySelector("#einheitSelect1").name =
+        "rezeptzutaten[" + (zaehler - 1) + "].einheit";
+      document
+        .getElementById("zutatenrow" + "_" + (zaehler + 1))
+        .querySelector("#zutatSelect1").name =
+        "rezeptzutaten[" + (zaehler - 1) + "].bezeichnung";
+
+      document
+        .getElementById("zutatenrow" + "_" + (zaehler + 1))
+        .querySelector("#minusbtn" + (zaehler + 1)).id = "minusbtn" + zaehler;
+
+      document.getElementById("zutatenrow" + "_" + (zaehler + 1)).id =
+        "zutatenrow" + "_" + zaehler;
+
+      zaehler = zaehler + 1;
+    }
+  } while (nodeAvailable);
+
+  /*
+  
+  */
+}
